309c754d647a76ba08975fd13ed6e71270bdf704
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-14 17:56:57 +0100

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,13 @@ var lista = document.getElementById('list');
 
 	add.addEventListener('click', function(list) {
 		checkOnclickEvent('dodaj')
+		var ul = document.getElementById('list');
+		ul.innerHTML += '<li>item 0</li>';
+
 	});
 	
 	lista.addEventListener('keypress', function(list) {
+  		e.target.value += ' <li>item + 1</li> ';
   		var list = document.createElement('li');
   			list.innerHTML += '<li>item + 1</li>';
 		console.log(list);
