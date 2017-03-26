d9ffe50cab528a9e14a06567369066bb537775f4
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-26 21:26:19 +0200

    correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,7 @@
-function checkOnclickEvent(listener) {
-	console.log(listener);
-}
-
 var lista = document.getElementById('list');
-	add = document.getElementById('addElem');
+	liElements = document.getElementsByTagName('li'),
+		add = document.getElementById('addElem');
 
-	add.addEventListener('click', function(list) {
-		checkOnclickEvent('dodaj')
-		var ul = document.getElementById('list');
-		ul.innerHTML += '<li>item 0</li>';
-
-	});
-	
-	lista.addEventListener('keypress', function(list) {
-  		e.target.value += ' <li>item + 1</li> ';
-  		var list = document.createElement('li');
-  			list.innerHTML += '<li>item + 1</li>';
-		console.log(list);
-	
-		var quantity = document.getElementsByTagName('li');
-  			console.log(quantity.length);
-	});
-
-
-
+add.addEventListener('click', function() {
+		list.innerHTML += '<li>item' + liElements.length + '</li>';
+});  			
